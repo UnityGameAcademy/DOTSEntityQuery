b4b4a23b0f2184e6e22e078b2d0ca00711365e00
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-11 11:06:33 -0700

    added AssignPlayerToTargetSystem, updated ChaserSubScene and SpaceshipSubScene

---
+++ Assets/Scripts/Systems/AssignPlayerToTargetSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class AssignPlayerToTargetSystem : SystemBase
+{
+
+    protected override void OnStartRunning()
+    {
+        base.OnStartRunning();
+        AssignPlayer();
+    }
+
+    protected override void OnUpdate()
+    {
+
+    }
+
+    private void AssignPlayer()
+    {
+        EntityQuery playerQuery = GetEntityQuery(ComponentType.ReadOnly<PlayerTag>());
+        Entity playerEntity = playerQuery.GetSingletonEntity();
+
+        Entities.
+            WithAll<ChaserTag>().
+            ForEach((ref TargetData targetData) =>
+            {
+                if (playerEntity != Entity.Null)
+                {
+                    targetData.targetEntity = playerEntity;
+                }
+
+            }).Schedule();
+    }
+}
