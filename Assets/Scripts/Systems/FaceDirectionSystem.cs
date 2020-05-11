773486b5adc2780038230ed210455c085f99151f
Wilmer Lin <unitygameacademy@gmail.com>
2020-05-11 11:05:47 -0700

    split TargetToDirectionSystem from FaceDirectionSystem

--- Assets/Scripts/Systems/FaceDirectionSystem.cs
+++ Assets/Scripts/Systems/FaceDirectionSystem.cs
@@ -7,31 +7,11 @@ public class FaceDirectionSystem : SystemBase
     protected override void OnUpdate()
     {
         Entities.
-            WithAll<PlayerTag>().
             ForEach((ref Rotation rot, in Translation pos, in MoveData moveData) =>
             {
                 FaceDirection(ref rot, moveData);
 
             }).Schedule();
-
-        Entities.
-            WithNone<PlayerTag>().
-            WithAll<ChaserTag>().
-            ForEach((ref MoveData moveData, ref Rotation rot, in Translation pos, in TargetData targetData) =>
-            {
-                ComponentDataFromEntity<Translation> allTranslations = GetComponentDataFromEntity<Translation>(true);
-                if (!allTranslations.Exists(targetData.targetEntity))
-                {
-                    return;
-                }
-
-                Translation targetPos = allTranslations[targetData.targetEntity];
-                float3 dirToTarget = targetPos.Value - pos.Value;
-                moveData.direction = dirToTarget;
-                FaceDirection(ref rot, moveData);
-
-            }).Run();
-
     }
 
     private static void FaceDirection(ref Rotation rot, MoveData moveData)
